620efa1db4f22f2db6b0defcee48ffc8c616ebe7
przestal <przemyslawg91@gmail.com>
2018-02-12 22:52:49 +0100

    TreeSet class added

---
+++ src/main/java/com/example/TreeSetExample.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import com.sun.xml.internal.bind.v2.TODO;
+
+import java.util.TreeSet;
+
+public class TreeSetExample {
+
+    public static void main(String[] args) {
+
+        TreeSet treeSet = new TreeSet();
+        treeSet.add(1);
+
+        // TODO: 12.02.18 check and give examples of the most important methods
+        System.out.println(treeSet.isEmpty());
+
+    }
+}
